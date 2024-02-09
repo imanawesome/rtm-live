4fc6294f002602b84eb7e8169fb6c2121d455936
imanawesome <101224687+imanawesome@users.noreply.github.com>
2024-02-09 12:28:52 +0800

    Create download_epg.sh

---
+++ download_epg.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -e "\e[1;31m [INFO] Downloading Aria2 \e[0m"
+wget https://github.com/P3TERX/Aria2-Pro-Core/releases/download/1.36.0_2021.08.22/aria2-1.36.0-static-linux-amd64.tar.gz
+
+echo -e "\e[1;31m [INFO] Extracting Aria2 \e[0m"
+tar -zxf aria2-1.36.0-static-linux-amd64.tar.gz
+ls -a 
+
+echo -e "\e[1;31m [INFO] Downloading EPG from weareblahs/epg \e[0m"
+rm *.xml
+chmod a+X aria2c
+./aria2c -x 10 -s 10 https://github.com/AqFad2811/epg/raw/main/rtmklik.xml
+
+echo -e "\e[1;31m [INFO] Cleaning up \e[0m"
+rm *.tar.gz
+rm aria2c
+
+echo -e "\e[1;31m [INFO] Generating Date & Time file in UTC+08 \e[0m"
+echo "`env TZ=UTC-08 date`" > "Date & Time UTC +08.txt"
+
+echo -e "\e[1;31m [INFO] Done \e[0m"
